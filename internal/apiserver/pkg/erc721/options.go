2f91aafc2bdd50c4ba605c221567e8af30e6224a
waite-lee <929401425@qq.com>
2022-04-16 20:46:56 +0800

    feat: 合并Proxy配置

--- internal/apiserver/pkg/erc721/options.go
+++ internal/apiserver/pkg/erc721/options.go
@@ -3,11 +3,9 @@ package erc721
 type EtherScanOptions struct {
 	ApiKey  string
 	BaseURL string
-	Proxy   string
 }
 
 var EsOptions = &EtherScanOptions{
 	BaseURL: "https://api.etherscan.io/api?",
 	ApiKey:  "ZZKAMSFGQ6KEFD6ZWM3PSI5UAQ724KSY75",
-	Proxy:   "http://localhost:4780",
 }
