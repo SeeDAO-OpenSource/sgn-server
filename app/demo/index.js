2fc6bfa68812b1a71c6b58e3ab82f4ce6da43a15
waite-lee <929401425@qq.com>
2022-04-17 22:43:47 +0800

    feat: 示例页面添加分页大小设置

--- app/demo/index.js
+++ app/demo/index.js
@@ -5,13 +5,13 @@ window.onload = (h, e) => {
     init()
 }
 
-const baseURL="http://localhost:5000"
+const baseURL = "http://localhost:5000"
 
 const init = async () => {
     const nftList = document.getElementById("ntfList");
     chidren = [...nftList.childNodes]
     chidren.forEach(n => nftList.removeChild(n))
-    const response = await fetch(`${baseURL}/api/v1/nft/0x23fDA8a873e9E46Dbe51c78754dddccFbC41CFE1?page=${page}&pageSize=${pageSize}`)
+    const response = await fetch(`${baseURL}/api/v1/nft/0x23fDA8a873e9E46Dbe51c78754dddccFbC41CFE1?page=${page}&page_size=${pageSize}`)
     const data = await response.json()
     if (data.success && data.data) {
         data.data.forEach(ntf => {
@@ -36,7 +36,7 @@ const createNtfItem = (data) => {
 
 const prePage = async () => {
     page--
-    if(page < 1) {
+    if (page < 1) {
         page = 1
     }
     await init()
@@ -45,4 +45,18 @@ const prePage = async () => {
 const nextPage = async () => {
     page++
     await init()
+}
+
+const setPageSize = async (size) => {
+    if (pageSize != size) {
+        pageSize = size
+        await init();
+    }
+}
+
+const submitPageSize = async () => {
+    const size = document.getElementById("pagesizeInput").value
+    if (size) {
+        await setPageSize(size)
+    }
 }
