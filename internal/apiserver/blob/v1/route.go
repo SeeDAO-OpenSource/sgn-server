aba6acb9f9fb26c7335f693abcfb8b96cfaa133c
waite-lee <waite_lee@outlook.com>
2022-04-19 20:56:32 +0800

    add resized image cache

--- internal/apiserver/blob/v1/route.go
+++ internal/apiserver/blob/v1/route.go
@@ -4,12 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func route(nftctl *BlobController, g *gin.Engine) {
+func route(blobCtroller *BlobController, g *gin.Engine) {
 	nftGroup := g.Group("/api/v1")
 	{
 		v1 := nftGroup.Group("blob")
 		{
-			v1.Any("*key", nftctl.Get)
+			v1.Any("*key", func(ctx *gin.Context) {
+				blobCtroller.Get(ctx)
+			})
 		}
 	}
 }
