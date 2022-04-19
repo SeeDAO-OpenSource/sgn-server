bdba9b82a3edb3c516518268278c957f25c4d4cc
waite-lee <waite_lee@outlook.com>
2022-04-19 20:56:32 +0800

    add resized image cache

--- pkg/blob/file/main.go
+++ pkg/blob/file/main.go
@@ -10,6 +10,8 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"github.com/nfnt/resize"
 	"github.com/waite-lee/nftserver/pkg/blob"
@@ -30,16 +32,16 @@ func NewFileBlobStore(options *FileBlobStoreOptions) blob.BlobStore {
 }
 
 func (f *FileBlobStore) Save(key *string, content *[]byte, overwrite bool) error {
-	ukey := convertKey(*key)
+	ukey := f.convertKey(*key)
 	_, err := os.Stat(f.options.BasePath)
 	if os.IsNotExist(err) {
 		dir, err := filepath.Abs(f.options.BasePath)
 		if err != nil {
 			return err
 		}
-		os.Mkdir(dir, os.ModePerm)
+		os.MkdirAll(dir, os.ModePerm)
 	}
-	if overwrite || !isExsits(filepath.Join(f.options.BasePath, ukey)) {
+	if overwrite || !f.isExsits(filepath.Join(f.options.BasePath, ukey)) {
 		path := filepath.Join(f.options.BasePath, ukey)
 		err = ioutil.WriteFile(path, *content, 0644)
 	}
@@ -47,19 +49,19 @@ func (f *FileBlobStore) Save(key *string, content *[]byte, overwrite bool) error
 }
 
 func (f *FileBlobStore) Exists(key *string) bool {
-	ukey := convertKey(*key)
+	ukey := f.convertKey(*key)
 	path := filepath.Join(f.options.BasePath, ukey)
-	return isExsits(path)
+	return f.isExsits(path)
 }
 
 func (f *FileBlobStore) Read(key *string, process *blob.Process) (*blob.BlobReader, error) {
-	ukey := convertKey(*key)
+	ukey := f.convertKey(*key)
 	path := filepath.Join(f.options.BasePath, ukey)
 	fileInfo, err := os.Stat(path)
 	if err != nil {
 		return nil, err
 	}
-	content, err := processFile(path, process)
+	content, err := f.processFile(path, process)
 	if err != nil {
 		return nil, err
 	}
@@ -70,27 +72,40 @@ func (f *FileBlobStore) Read(key *string, process *blob.Process) (*blob.BlobRead
 	}, nil
 }
 
-func isExsits(path string) bool {
+func (f *FileBlobStore) isExsits(path string) bool {
 	_, err := os.Stat(path)
 	return !os.IsNotExist(err)
 }
 
-func convertKey(key string) string {
+func (f *FileBlobStore) convertKey(key string) string {
 	return url.QueryEscape(key)
 }
 
-func processFile(path string, process *blob.Process) ([]byte, error) {
-	var content []byte
-	var err error
+func (f *FileBlobStore) processFile(path string, process *blob.Process) ([]byte, error) {
 	if process == nil {
-		content, err = ioutil.ReadFile(path)
+		return ioutil.ReadFile(path)
+	}
+	cachPath := f.getCachPath(path, process)
+	if cachPath != "" {
+		content, err := os.ReadFile(cachPath)
+		if err == nil {
+			return content, nil
+		}
 	}
 	file, err := os.Open(path)
 	defer file.Close()
 	if err != nil {
 		return nil, err
 	}
+	content, err := f.resizeImage(file, process)
+	if err != nil {
+		content, err = ioutil.ReadFile(path)
+	}
+	err = os.WriteFile(cachPath, content, 0644)
+	return content, err
+}
 
+func (f *FileBlobStore) resizeImage(file *os.File, process *blob.Process) ([]byte, error) {
 	img, format, err := image.Decode(file)
 	log.Println(format)
 	if err == nil {
@@ -103,11 +118,21 @@ func processFile(path string, process *blob.Process) ([]byte, error) {
 			case "jpeg":
 				err = jpeg.Encode(&buf, m, nil)
 			}
-			content = buf.Bytes()
+			return buf.Bytes(), nil
 		}
 	}
-	if len(content) == 0 {
-		content, err = ioutil.ReadFile(path)
+	return nil, err
+}
+
+func (f *FileBlobStore) getCachPath(path string, process *blob.Process) string {
+	if process.Width > 0 && process.Height > 0 {
+		ext := filepath.Ext(path)
+		fileName := strings.TrimSuffix(filepath.Base(path), ext)
+		dir := filepath.Join(filepath.Dir(path), ".cache", fileName)
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err == nil {
+			return filepath.Join(dir, strconv.Itoa(process.Width)+"_"+strconv.Itoa(process.Height)+ext)
+		}
 	}
-	return content, err
+	return ""
 }
